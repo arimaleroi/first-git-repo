dc0e14ac618eddf1c288519eb8ab4064aa512862
sergey <dazy.shakespeare@gmail.com>
2022-12-14 19:23:18 +0200

    Revert "Revert "add homework 2_1""
    This reverts commit 2f3a440f0d585b87d684e1b802ea42bc2389ffae.

---
+++ HW_2_1_Chernysh/HW_2_1_Chernysh/Actions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_1_Chernysh
+{
+    public static class Actions
+    {
+        public static Result InfoAction()
+        {
+            return new Result(StatusEnum.Info, "Message Info", DateTime.Now);
+        }
+
+        public static Result WarningAction()
+        {
+            return new Result(StatusEnum.Warning, "Message Warning", DateTime.Now);
+        }
+
+        public static Result ErrorAction()
+        {
+            return new Result(StatusEnum.Error, "Message Error", DateTime.Now);
+        }
+    }
+
+}
---
+++ HW_2_2_Chernysh/HW_2_2_Chernysh/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HW_2_2_Chernysh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
