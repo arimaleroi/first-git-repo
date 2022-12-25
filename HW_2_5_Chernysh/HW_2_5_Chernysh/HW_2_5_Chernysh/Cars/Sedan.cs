885837b794871efb4b6c31efafa07f6dbdb4b027
sergey <dazy.shakespeare@gmail.com>
2022-12-25 21:51:45 +0200

    add hw 2_5

---
+++ HW_2_5_Chernysh/HW_2_5_Chernysh/HW_2_5_Chernysh/Cars/Sedan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_3_Chernysh.Cars
+{
+    internal class Sedan : Car
+    {
+        public Sedan(int maxSpeed, int weight) : base(maxSpeed, weight)
+        {
+
+            TypeOfCar = "Sedan";
+        }
+    }
+}
---
+++ HW_2_5_Chernysh/HW_2_5_Chernysh/HW_2_5_Chernysh/Motorbikes/Scooter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_3_Chernysh.Motorbikes
+{
+    internal class Scooter : Motorbike
+    {
+        public Scooter(int maxSpeed, int weight) : base(maxSpeed, weight)
+        {
+
+            TypeOfMotorbike = "Scooter";
+        }
+    }
+}
