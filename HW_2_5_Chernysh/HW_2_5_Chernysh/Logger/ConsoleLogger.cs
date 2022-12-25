77d5845fdc634f7dda4e4f2166fe2eb8039775b2
sergey <dazy.shakespeare@gmail.com>
2022-12-25 21:44:16 +0200

    add hw 2_5

---
+++ HW_2_5_Chernysh/HW_2_5_Chernysh/Logger/ConsoleLogger.cs
@@ -0,0 +1,23 @@
+﻿using HW_2_3_Chernysh.Logger;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using System.Text.Unicode;
+using System.Threading.Tasks;
+
+namespace HW_2_3_Chernysh
+{
+    public class ConsoleLogger : ILogger
+    {
+        public void AddLog(Result log)
+        {
+
+
+            Console.WriteLine(Json.JsonLogger(log));
+        }
+    }
+}
---
+++ HW_2_5_Chernysh/HW_2_5_Chernysh/Logger/Result.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_3_Chernysh
+{
+    public struct Result
+    {
+        public StatusEnum Status { get; set; }
+
+        public string Message { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public Result(StatusEnum status, string message, DateTime dateTime)
+        {
+            Status = status;
+            Message = message;
+            DateTime = dateTime;
+        }
+
+        public override string ToString()
+        {
+            return $"{DateTime}: {Status}: {Message}";
+        }
+    }
+
+    public enum StatusEnum
+    {
+        Error = 0,
+        Info = 1
+    }
+
+}
