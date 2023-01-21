425130dcb5ff3ab1479c8df4bae0c4f856d8ad61
sergey <dazy.shakespeare@gmail.com>
2023-01-21 15:36:42 +0200

    add hw 3_3

---
+++ HW3_3Chernysh/HW3_3Chernysh/Newspapers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3_3Chernysh
+{
+    public class Newspapers
+    {
+        public string NameOfNewspaper { get; set; }
+        public string Headline { get; set; }
+        public string Information { get; set; }
+
+        public Newspapers(string nameOfNewspaper, string headline, string information)
+        {
+            NameOfNewspaper = nameOfNewspaper;
+            Headline = headline;
+            Information = information;
+        }
+    }
+}
