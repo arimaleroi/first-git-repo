02a5a3594eacba79f6b8185ade7a1809c6e38f03
sergey <dazy.shakespeare@gmail.com>
2023-02-08 20:15:36 +0200

    Revert "Delete HW3_2_Chernysh directory"
    This reverts commit 5d35651bc529225b53dd0f628c18c34f111bc307.

---
+++ HW3_2_Chernysh/HW3_2_Chernysh/UserLinkedList.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3_2_Chernysh
+{
+    internal class UserLinkedList<T> : IEnumerable<T>
+    {
+        public Item<T> Head { get; private set; }
+        public Item<T> Tail { get; private set; }
+
+        public int Count { get; private set; }
+
+        public UserLinkedList()
+        {
+            Head = null;
+            Tail = null;
+            Count = 0;
+        }
+
+        public UserLinkedList(T data)
+        {
+            HeadAndTail(data);
+        }
+
+        public void Add(T data)
+        {
+            var item = new Item<T>(data);
+
+            if(Tail != null)
+            {
+                Tail.Next = item;
+                Tail = item;
+                Count++;
+            }
+            else
+            {
+                HeadAndTail(data);
+            }
+        }
+
+        public void Remove(T data)
+        {
+            if (Head != null)
+            {
+                if(Head.Data.Equals(data))
+                {
+                    Head = Head.Next;
+                    Count--;
+                    return;
+                }
+
+                var current = Head.Next;
+                var previous = Head;
+
+                while(current != null)
+                {
+                    if(current.Data.Equals(data))
+                    {
+                        previous.Next = current.Next;
+                        Count--;
+                        return;
+                    }
+
+                    previous = current;
+                    current = current.Next;
+                }
+            }
+        }
+
+        public bool Search(T data)
+        {
+            var current = Head;
+            while (current!=null)
+            {
+                if (current.Data.Equals(data))
+                {
+                    return true;
+                }
+                current = current.Next;
+            }
+            return false;
+        }
+
+        private void HeadAndTail(T data)
+        {
+            var item = new Item<T>(data);
+            Head = item;
+            Tail = item;
+            Count = 1;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var current = Head;
+            while(current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HW3_2_Chernysh/HW3_2_Chernysh/WhereEqualsExtension.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW3_2_Chernysh
+{
+    public static class Extensions
+    {
+        public static IEnumerable<T> WhereEquals<T>(this IEnumerable<T> values, T a)
+        {
+            foreach (var item in values)
+            {
+                if (a.Equals(item))
+                    yield return item;
+            }
+        }
+    }
+}
