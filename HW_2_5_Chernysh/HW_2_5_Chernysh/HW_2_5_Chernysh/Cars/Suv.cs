a8fbdc01fe768610c8005ab44740c23ef30b94db
sergey <dazy.shakespeare@gmail.com>
2023-02-08 20:14:55 +0200

    Revert "Delete HW_2_5_Chernysh directory"
    This reverts commit ad60aa569e10b86f3ebfd4d8785ac81a6cb21489.

---
+++ HW_2_5_Chernysh/HW_2_5_Chernysh/HW_2_5_Chernysh/Cars/Suv.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_3_Chernysh.Cars
+{
+    internal class Suv : Car
+    {
+        public Suv(int maxSpeed, int weight) : base(maxSpeed, weight)
+        {
+            TypeOfCar = "Suv";
+        }
+    }
+}
---
+++ HW_2_5_Chernysh/HW_2_5_Chernysh/HW_2_5_Chernysh/Motorbikes/Scooter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW_2_3_Chernysh.Motorbikes
+{
+    internal class Scooter : Motorbike
+    {
+        public Scooter(int maxSpeed, int weight) : base(maxSpeed, weight)
+        {
+            TypeOfMotorbike = "Scooter";
+        }
+    }
+}
